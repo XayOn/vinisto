ddfc4ce19dccb30d9befc7ad34ac49be38c4f595
David Francos Cuartero <dfrancos@buguroo.com>
2015-09-08 13:19:29 +0200

    Added parameter handling

--- vinisto/main.py
+++ vinisto/main.py
@@ -4,7 +4,14 @@
     Main
 """
 
+import inspect
 import logging
+import pkgutil
+import argparse
+import vinisto.plugins
+import vinisto.tts
+import vinisto.stt
+from importlib import import_module
 
 
 logging.basicConfig(level=logging.DEBUG)
@@ -24,10 +31,12 @@ class Vinisto(object):
         """
             Register a plugin
         """
+        LOG.info("Registering plugin {}".format(plugin))
         self.plugins.append(plugin(self))
 
     def wait_for_keyword(self, keyword):
         while True:
+            LOG.info("Wating for keyword")
             if next(self.stt.text) == keyword:
                 break
 
@@ -41,41 +50,84 @@ class Vinisto(object):
             pass
         for plugin in self.plugins:
             try:
+                LOG.info('Calling plugin {} callback'.format(plugin))
                 plugin.callback(text)
             except:
                 # We can't allow a bad plugin to break anything =(
                 pass
 
 
+def extract_modules(module, nam):
+    """
+        Get vinisto modulenames for a specific submodule.
+    """
+    modules = pkgutil.iter_modules(module.__path__)
+    return ["vinisto.{}.{}".format(nam, mod) for _, mod, _ in modules]
+
+
+def extract_classes(plugins):
+    """
+        Extracts all classes in
+    """
+    for module in plugins:
+        try:
+            module = import_module(module)
+            for class_ in inspect.getmembers(module, inspect.isclass):
+                if class_[1].__module__.startswith('vinisto.'):
+                    yield class_[1]
+        except RuntimeError:
+            pass
+
+
 def main():
     """
-        Usually this should not be used,
-        you must implement this part yourself.
-
-        Have a nice day =)
+        Main CLI.
+        This receives as parameters the vinisto modules to load.
     """
+    vinisto_ = Vinisto()
 
-    from vinisto.tts.google_tts import GoogleTTS
-    from vinisto.stt.google_stt import GoogleSTT
-    from vinisto.plugins.time import Time
-    from vinisto.plugins.repeat import Repeat
+    plugins = extract_modules(vinisto.plugins, 'plugins')
+    tts_plugins = extract_modules(vinisto.tts, 'tts')
+    stt_plugins = extract_modules(vinisto.stt, 'stt')
 
-    vinisto = Vinisto()
-    vinisto.stt = GoogleSTT('es-ES', {'rate': 24000},  # RPI external mic =)
-                            "AIzaSyCuOvb2qd0mhQRkIbGAcgMUmFQaLIXtlmg")
-    vinisto.tts = GoogleTTS('es-ES')
-    vinisto.register_plugin(Time)
-    vinisto.register_plugin(Repeat)
-    try:
-        from vinisto.plugins.gpio import Gpio
-        vinisto.register_plugin(Gpio)
-    except RuntimeError:
-        pass
+    parser = argparse.ArgumentParser(
+        description='Vinisto - Your personal butler')
 
+    parser.add_argument('--plugins',  type=str, nargs='?',
+                        choices=plugins,
+                        help='List of plugin modules',
+                        default=plugins)
+
+    parser.add_argument('--stt',  type=str,
+                        choices=stt_plugins,
+                        help='List of stt modules',
+                        default='vinisto.stt.google_stt')
+
+    parser.add_argument('--tts',  type=str,
+                        choices=tts_plugins,
+                        help='List of tts modules',
+                        default='vinisto.tts.google_tts')
+
+    parser.add_argument('--keyword',  type=str,
+                        help='Keyword to wait for',
+                        default='vinisto')
+
+    args = parser.parse_args()
+
+    TTS = next(extract_classes([args.tts]))
+    STT = next(extract_classes([args.stt]))
+
+    vinisto_.stt = STT(language='es-ES', rate=24000,
+                       key="AIzaSyCuOvb2qd0mhQRkIbGAcgMUmFQaLIXtlmg")
+    vinisto_.tts = TTS(language='es-ES')
+
+    for class_ in extract_classes(plugins):
+        vinisto_.register_plugin(class_)
 
     while True:
-        vinisto.wait_for_keyword('vinisto')
-        vinisto.execute_callbacks()
+        vinisto_.wait_for_keyword(args.keyword)
+        LOG.info("Keyword recognized")
+        vinisto_.execute_callbacks()
 
 
 if __name__ == "__main__":
