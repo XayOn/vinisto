6bb9aaf2c04dffe3c392e74beca298dfd319f99d
David Francos Cuartero <dfrancos@buguroo.com>
2015-11-07 01:45:41 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('docs/History.rst') as history_file:
 
 setup(
     name='vinisto',
-    version='0.1.3',
+    version='0.1.4',
     description="""
         Vinisto - a simple-minded home butler
     """,
