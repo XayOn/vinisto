f06de93cdc4338f830340cdc73d9fac7e14b676a
David Francos Cuartero <dfrancos@buguroo.com>
2017-03-11 18:31:15 +0100

    Deleted old tests

--- tests/test_nltk_base.py
+++
@@ -1,6 +0,0 @@
-def test_phrase_nltk():
-    from vinisto.intent import Intent
-    from vinisto.language import Phrase
-
-    pra = Phrase("Butler, open the windows")
-    assert len(pra.phrase) == 5
