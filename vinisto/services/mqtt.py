94caabdb11249885000c1dbaba83ab5cb4abb798
David Francos Cuartero <dfrancos@buguroo.com>
2017-03-30 05:07:29 +0200

    Fixed MQTT connector

--- vinisto/services/mqtt.py
+++ vinisto/services/mqtt.py
@@ -9,11 +9,12 @@ from vinisto.services import RestConnector
 
 def run():
     """Connect to the mqtt server and send every sensor event to the API"""
+    connector = RestConnector()
 
     def setup_client(client):
         """ Setup the mqtt client """
         client.engine.mqtt = client
-        client.on_message = lambda c, _, msg: RestConnector.send_messages(msg)
+        client.on_message = lambda c, _, msg: connector.update_sensors(msg)
         client.on_disconnect = lambda c, u, r: c.reconnect()
 
     mqtt_client(setup_client)
